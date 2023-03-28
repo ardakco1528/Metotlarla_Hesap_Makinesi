2175ef4b85c9d7bbc35e2bc514df51607949690f
Arda Koca <110680382+ardakco1528@users.noreply.github.com>
2023-03-28 20:10:47 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,38 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // kullanıcıdan sayılara alacagımız bir metod.
+        // yapılacak islemi sececegimiz bir metod.
+        // islemler icin bir metod.
+        int sayı1 = Sayı_al();
+        int sayı2 = Sayı_al();
+        String ıslem = Islem_sec();
+        double sonuc = Islemler(sayı1,sayı2,ıslem);
+        System.out.println("Sonuc = " +sonuc);
+    }
+    public static int Sayı_al() {
         Scanner scanner = new Scanner(System.in);
-        int sayı1,sayı2;
-
-        System.out.print("İslem yapmak icin 1.sayıyı  girin: ");
-        if(scanner.hasNextInt()){ // kullanıcın tam sayı girip girmedigini kontrol ediyoruz...
-            sayı1 = scanner.nextInt();
-        }else {
-            System.out.println("Lutfen bir tam sayı giriniz...");
-            return;
-        }
-        System.out.print("İslem yapmak icin 2.sayıyı  girin: ");
-        if(scanner.hasNextInt()){
-            sayı2 = scanner.nextInt();
-            System.out.println("Dogru bir deger girdiniz.");
-        }else {
-            System.out.println("Lutfen bir tam sayı giriniz...");
-            return;
-        }
-        Toplama(sayı1,sayı2); 
-        Cıkarma(sayı1,sayı2);
-        Carpma(sayı1,sayı2);
-        Bolme(sayı1,sayı2);
+        System.out.println("Bir sayı giriniz: ");
+        return scanner.nextInt();
     }
-    public static int Toplama(int a, int b){
-        System.out.println("Toplama isleminin sonucu = " + (a + b));
-        return a + b;
+    public static String Islem_sec(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Islemlerden birini seciniz: +,-,*,/");
+        return scanner.nextLine();
     }
-    public static void Cıkarma(int c, int d){
-        System.out.println("Cıkarma isleminin sonucu = " + (c - d));
-    }
-    public static void Carpma(int e,int f){
-        System.out.println("Carpma isleminin sonucu = " + (e * f));
-    }
-    public static int Bolme(int g, int h) {
-        if (h == 0) {
-            System.out.println("Hatalı bolme islemi.Lutfen sıfırdan farklı bir sayı girin...");
-        } else {
-            System.out.println("Bolme isleminin sonucu = " + (g / h));
-        }
-        return g / h;
-    }
+    public static double Islemler(int sayı1, int sayı2, String islem){
+            switch (islem) {
+                case "+":
+                    return sayı1 + sayı2;
+                case "-":
+                    return sayı1 - sayı2;
+                case "*":
+                    return sayı1 * sayı2;
+                case "/":
+                    return (double) sayı1 / sayı2;
+                default:
+                    System.out.println("Yanlıs bir islem sectiniz...");
+                    return 8;
+            }
+       }
 }
